63285bedc8dc259365b3ed45a4be6efade3403d4
Robert <rothattack@gmail.com>
2017-02-06 11:08:35 -0600

    Upload files i've previously worked on.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp')
+var mjml = require('gulp-mjml')
+var browserSync = require('browser-sync');
+
+// Creates a localhost server
+var server = function() {
+	browserSync({
+		server: {
+			baseDir: ['./html']
+		},
+		startPath: ''
+	})
+}
+
+// Render our MJML to HTML
+gulp.task('build', () => {
+  return gulp.src('./mjml/**/*.mjml')
+    .pipe(mjml())
+    .pipe(gulp.dest('./html'))
+})
+
+// Launch our HTML after rendering
+gulp.task('default', gulp.series('build', () => {
+	server();
+	gulp.watch('./mjml/**/*.mjml')
+		.on('change', gulp.series('build', browserSync.reload))
+}))
